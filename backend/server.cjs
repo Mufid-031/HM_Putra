f1cf8563f49fc58b84af06a3d1d314a175513389
Mufid-031 <mufidmaxsus17@gmail.com>
2024-08-01 23:42:01 +0700

    tes

--- backend/server.cjs
+++ backend/server.cjs
@@ -23,6 +23,12 @@ db.once('open', () => {
 
 app.use('/api', require('./routes/api.cjs'))
 
+app.use(express.static(path.join(__dirname, './dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './dist', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
